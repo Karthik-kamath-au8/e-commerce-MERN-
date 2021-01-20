2597ebea86cc8fa5af5dfe0db016b0dc03f45d66
karthik <kamathkarthik13@gmail.com>
2021-01-21 00:32:09 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import {Container} from "react-bootstrap"; 
+import {BrowserRouter, Route} from "react-router-dom"
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <>
+    
+    <BrowserRouter>
     <Header/>
     <main className="py-3">
       <Container>
-      <h1>Welcome to e-commerce</h1>
+        <Route path='/' component={HomeScreen} />
       </Container>
     
     </main>
     <Footer/>
      
      
-    </>
+   
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+{
+    _id:'1',
+    name:'Airpods Wireless Bluetooth Headphones',
+    image:'/images/airpods.jpg',
+    description:
+    'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you take calls while working',
+    brand:'Apple',
+    category:'Electronics',
+    price:89.90,
+    countInStock:3,
+    rating:4.5,
+    numReviews:4,
+
+},
+{
+    _id:'2',
+    name:'Iphone 11 256GB Memory',
+    image:'/images/phone.jpg',
+    description:
+    'Introducing the Iphone 11. A transformative triple-camera system that adds tons of capability without complexity.An unprecendented leap in battery',
+    brand:'Apple',
+    category:'Electronics',
+    price:599.90,
+    countInStock:19,
+    rating:4.0,
+    numReviews:4,
+},
+{
+    _id:'3',
+    name:'Cannon EOS 80D DSLR Camera',
+    image:'/images/camera1.jpg',
+    description:
+    'Characterized by versatile imaging specs,the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intutive design',
+    brand:'Cannon',
+    category:'Electronics',
+    price:929.90,
+    countInStock:5,
+    rating:3.0,
+    numReviews:3,  
+},
+{
+    _id:'4',
+    name:'Sony Playstation 4 Pro',
+    image:'/images/playstation1.jpg',
+    description:
+    'The ultimate home entertainment center starts with Playstation. Whether you are into gaming,HD movies,television,music',
+    brand:'Sony',
+    category:'Electronics',
+    price:399.90,
+    countInStock:10,
+    rating:5.0,
+    numReviews:3,  
+},
+{
+    _id:'5',
+    name:'Logitech G-series Gaming Mouse',
+    image:'/images/mouse1.jpg',
+    description:
+    'Get a better  handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand:'Logitech',
+    category:'Electronics',
+    price:49.90,
+    countInStock:7,
+    rating:3.5,
+    numReviews:2, 
+},
+{
+    _id:'6',
+    name:'Amazon Echo Dot 3rd Generation',
+    image:'/images/alexa.jpg',
+    description:
+    'Meet Echo Dot _ Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space ',
+    brand:'Amazon',
+    category:'Electronics',
+    price:29.90,
+    countInStock:0,
+    rating:4.0,
+    numReviews:4, 
+}
+]
+export default products;
