4a66bb31b734df5595fea3aebe814cfa8ad997d4
karthik <kamathkarthik13@gmail.com>
2021-01-22 00:27:56 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route} from "react-router-dom"
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
     <Header/>
     <main className="py-3">
       <Container>
-        <Route path='/' component={HomeScreen} />
+        <Route exact path='/' component={HomeScreen} />
+        <Route path='/product/:id' component={ProductScreen} />
       </Container>
     
     </main>
